ce44452269b67b95fe1031fd82e12cacbabf3d44
Shikhar9960 <shikharjais5@gmail.com>
2025-01-15 00:52:57 +0530

    first commit

--- api/index.js
+++ api/index.js
@@ -1,41 +1,64 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors'; // Importing CORS
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
-import listingRouter from './routes/listing.route.js'
+import path from 'path';
+
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
-    console.log("connected to mongodb")
-}).catch((err) => {
-    console.log(err)
-})
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-
+const __dirname = path.resolve();
 const app = express();
+
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3000, () => {
-    console.log('service is running  on port 3000!');
-});
+// CORS Configuration
+const corsOptions = {
+  origin: 'https://cozycorners9960.netlify.app', // Replace with your frontend URL
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, // Allow cookies and other credentials
+};
+app.use(cors(corsOptions));
 
+// API routes
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
+// Serve static files for frontend
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.use((err, req, res, next) => {
-    const statuscode = err.statuscode || 500;
-    const message = err.message || 'internal server error';
-    return res.status(statuscode).json({
-        success: false,
-        statuscode,
-        message
-    });
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
+// Start server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
