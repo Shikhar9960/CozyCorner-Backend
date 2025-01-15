f6c7c73c7e088d117156721351bce37f3888ad3e
Shikhar9960 <shikharjais5@gmail.com>
2025-01-15 20:40:21 +0530

    Your commit message

--- api/index.js
+++ api/index.js
@@ -7,7 +7,6 @@ import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
-
 dotenv.config();
 
 mongoose
@@ -19,14 +18,14 @@ mongoose
     console.log(err);
   });
 
-const __dirname = path.resolve();
+  const __dirname = path.resolve();
+
 const app = express();
 
-// Middleware
 app.use(express.json());
+
 app.use(cookieParser());
 
-// CORS Configuration
 const corsOptions = {
   origin: 'https://cozycorners9960.netlify.app', // Replace with your frontend URL
   methods: 'GET,POST,PUT,DELETE',
@@ -35,19 +34,21 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-// API routes
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
+
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
-// Serve static files for frontend
+
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
+})
 
-// Error handling middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
@@ -58,7 +59,3 @@ app.use((err, req, res, next) => {
   });
 });
 
-// Start server
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
-});
